26bf25f225e619326a7c94f5f2d349d0eb9e4d67
BogdanGura <144367057+BogdanGura@users.noreply.github.com>
2023-11-02 14:24:15 -0400

    Delete chatting-app directory

--- chatting-app/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function sanitize($data)
-{
-   $data = trim($data);
-   return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
-}
-
-?>
--- chatting-app/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-//When user logs out, clear cache
-session_start();
-session_destroy();
-
-//The redirect the user to the login page
-header("Location: /chatting-app/login.php");
-?>
